5ae8ef44af2e4fadde28565ea59db2bdfb3ea431
yurigrelu78-lab <yurigrelu78@gmail.com>
2026-08-21 23:39:07 +0200

    Update Kotlin and Java compatibility in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,9 +22,13 @@ android {
 
     
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+    compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+kotlinOptions {
+    jvmTarget = "17"
+}
 }
 
 dependencies {
