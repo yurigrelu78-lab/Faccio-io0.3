92ce9c916c544bca144fe482919222e657dfa74b
yurigrelu78-lab <yurigrelu78@gmail.com>
2026-08-22 17:34:48 +0200

    Correggi la compilazione della configurazione iniziale

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ kotlinOptions {
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.9.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.4")
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
