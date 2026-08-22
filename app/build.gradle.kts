e71a475a4dfd9651a9eb0720e3022ab7d62a33ad
yurigrelu78-lab <yurigrelu78@gmail.com>
2026-08-22 19:54:20 +0200

    Aggiungi le icone per la ricerca

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("com.google.android.gms:play-services-location:21.3.0")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
