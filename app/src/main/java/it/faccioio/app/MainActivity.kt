8b02a8460370a6290b0fa43ad5817b4222edbd00
yurigrelu78-lab <yurigrelu78@gmail.com>
2026-08-24 19:59:37 +0200

    Fix compilazione filtri orizzontali

--- app/src/main/java/it/faccioio/app/MainActivity.kt
+++ app/src/main/java/it/faccioio/app/MainActivity.kt
@@ -29,6 +29,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
